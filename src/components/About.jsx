f5834a69ac2766d8ef5935734452cb3d737e5515
samuelberhane <1samibrhane@gmail.com>
2023-03-18 09:51:04 +0300

    Create video image content of About section.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { blogs } from "../utils/blogPosts";
 import BlogCard from "./BlogCard";
+import { AiFillCheckCircle } from "react-icons/ai";
+import play from "../assets/play.gif";
+import blob from "../assets/blob1.svg";
+import blob1 from "../assets/blob2.svg";
+import blob2 from "../assets/blob3.svg";
 
 const About = () => {
   return (
@@ -17,6 +22,74 @@ const About = () => {
         {blogs.map((blog, index) => (
           <BlogCard key={index} blog={blog} />
         ))}
+      </div>
+
+      {/* How it works */}
+      <div className="flex flex-col lg:flex-row lg:gap-5 xl:gap-[60px] mt-7 mb-14">
+        <div className="relative shadow-md lg:w-[50%] h-[470px]">
+          <img
+            src="https://images.pexels.com/photos/7441079/pexels-photo-7441079.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="videoImage"
+            className="h-[470px] rounded-md z-20"
+          />
+          <img
+            src={play}
+            alt="playImage"
+            className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[120px] h-[120px] rounded-full"
+          />
+          <div className="absolute -bottom-14 -left-20 -z-30">
+            <img
+              src={blob}
+              alt="playImage"
+              className="w-[300px] h-[330px] z-0"
+            />
+          </div>
+          <div className="absolute -bottom-32 -left-10 -z-10 ">
+            <img
+              src={blob1}
+              alt="playImage"
+              className="w-[400px] h-[330px] z-0"
+            />
+          </div>
+          <div className="absolute -bottom-28 -left-20 -z-20 rotate-180">
+            <img
+              src={blob2}
+              alt="playImage"
+              className="w-[400px] h-[330px] z-0"
+            />
+          </div>
+        </div>
+        <div className="lg:w-[50%]">
+          <h1 className="text-[#b22fae] font-semibold text-xl">
+            WHATS THE FUNCTION
+          </h1>
+          <h1 className=" font-bold text-xl mt-3 mb-6 lg:mb-8">
+            Let&#39;s see how it works
+          </h1>
+          <h1 className="text-xl font-light lg:text-4xl xl:text-[50px]">
+            We help you to boost your business profitable with our talent
+            marketer
+          </h1>
+
+          <p className="mt-3 lg:mt-5">
+            Get your tests delivered at let home collect sample sample from the
+            victory of the managements that supplies best design system
+            guidelines ever.
+          </p>
+
+          <div className="flex gap-1 items-center mt-3 lg:mt-5">
+            <AiFillCheckCircle className="text-[#23a2ad] text-xl" />
+            <p className="text-lg font-semibold">
+              Unlimited design possibility
+            </p>
+          </div>
+          <div className="flex gap-1 items-center">
+            <AiFillCheckCircle className="text-[#23a2ad] text-xl" />
+            <p className="text-lg font-semibold">
+              Completely responsive features
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
